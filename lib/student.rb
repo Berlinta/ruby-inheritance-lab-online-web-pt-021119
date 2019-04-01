3306b6eca9ec468cdf3f98d0b2c3147bd54cbb14
Nash Brancic <nashfft@gmail.com>
2019-04-01 23:05:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ attr_accessor :first_name, :last_name
 def initialize(first_name, last_name, knowledge)
   @first_name = first_name
   @last_name = last_name
-  @@knowledge = []
+  @@knowledge < self
 end
 
 def learn(knowledge)
